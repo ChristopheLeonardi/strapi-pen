396ad7e50984a2f39ae8d60132e6393f86a48e63
christopheLeonardi <christophe.leonardi1@gmail.com>
2025-03-11 12:28:14 +0100

    save

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://admin.planetearthnow.org'), // Ajoutez l'URL publique pour les redirections
+  url: env('PUBLIC_URL', 'http://localhost:1337'), // Ajoutez l'URL publique pour les redirections
   admin: {
-    url: env('ADMIN_URL', 'http://admin.planetearthnow.org/admin'), // URL admin si différente
+    url: env('ADMIN_URL', 'http://localhost:1337/admin'), // URL admin si différente
   },
   app: {
     keys: env.array('APP_KEYS'),
